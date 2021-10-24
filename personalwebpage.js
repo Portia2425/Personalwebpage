37c2827c13ee613d1d569f369b9ae4775d23a8d0
Portia <portia.ngaha25@gmail.com>
2021-10-24 15:10:04 +1300

    faq section and light editing

--- personalwebpage.js
+++ personalwebpage.js
@@ -20,3 +20,42 @@ document.querySelector('.close').addEventListener('click',
 function() {
   document.querySelector('.contactUs').style.display = 'none';
 });
+
+// accordian faq section
+// quesiton 1 button
+function faqButton(one) {
+  let x = document.getElementById(one);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  };
+} 
+
+// quesiton 2 button
+function faqButton(two) {
+  let x = document.getElementById(two);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+// quesiton 3 button
+function faqButton(three) {
+  let x = document.getElementById(three);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+// quesiton 4 button
+function faqButton(four) {
+  let x = document.getElementById(four);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
