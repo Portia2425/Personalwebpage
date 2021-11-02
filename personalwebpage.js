40eec046b8aebc4cc1c6d1ba978d3e608af6c90d
Portia <portia.ngaha25@gmail.com>
2021-11-03 02:24:40 +1300

    final editing of code

--- personalwebpage.js
+++ personalwebpage.js
@@ -1,23 +1,42 @@
 // scrollable navbar
-let prevScrollPos = window.pageYOffset;
+let previousPosition = window.pageYOffset;
 window.onscroll = function() {
-  let currentScroll = window.pageYOffset;
-  if (prevScrollPos > currentScroll) {
+  let currentPosition = window.pageYOffset;
+  
+  if (previousPosition > currentPosition) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-200px";
   }
-  prevScrollPos = currentScroll;
+  previousPosition = currentPosition;
 } 
 
 // contact us modal
-function popupOpen() {
+function modalOpen() {
   document.querySelector('.contactUs').style.display = 'flex';
 };
 
-function popupClose() {
+function modalClose() {
   document.querySelector('.contactUs').style.display = 'none';
 }
+
+// new zealand image rollover
+let images = ["img/nz_2.png", "img/nz_3.png", "img/nz_4.png", "img/nz_5.png"] 
+let i = 0;
+
+function rollover (id_image, sec) {
+  let image = document.getElementById(id_image);
+  image.src = images[i];
+  i++;
+
+  if (i >= images.length) {
+    i = 0;
+  } 
+
+  setTimeout("rollover('" + id_image + "'," + sec + ");", sec);
+}
+
+rollover("image1",2000);
 
 
 // faq section
@@ -35,54 +54,48 @@ document.querySelectorAll('.accordian__button').forEach(button => {
   });
 });
 
-// new zealand image rollover
-let images = new Array ("img/nz_2.png","img/nz_3.png", "img/nz_4.png", "img/nz_5.png")
-
-let i = 0;
-
-function rollover (image_id, millisecs) {
-  let image = document.getElementById(image_id);
-  image.src = images[i];
-  i++;
-
-  if (i >= images.length) {
-    i = 0;
-  } 
-
-  setTimeout("rollover('" + image_id + "'," + millisecs + ");",millisecs);
-}
-
-rollover("image1",2000);
-
 // contact form validation
-function validate(){
+function validate() {
   let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let message = document.getElementById("message").value;
-  let error_message = document.getElementById("error_message");
+  let error_message_name = document.getElementById("error_message_name");
+  let error_message_email = document.getElementById("error_message_email");
+  let error_message_message = document.getElementById("error_message_message");
   
   let text;
   
   if(name.length < 3){
     text = "Please enter valid name";
-    error_message.innerHTML = text;
+    error_message_name.innerHTML = text;
+    document.getElementById("name").style.borderColor = "red";
     return false;
+  } else {
+    error_message_name.innerHTML = ""
+    document.getElementById("name").style.borderColor = "white";
   };
 
   if(email.indexOf("@") === -1 || email.length < 5){
     text = "Please enter valid email";
-    error_message.innerHTML = text;
+    error_message_email.innerHTML = text;
+    document.getElementById("email").style.borderColor = "red";
     return false;
+  } else {
+    error_message_email.innerHTML = ""
+    document.getElementById("email").style.borderColor = "white";
   };
 
   if(message.length <= 30){
     text = "Please enter more than 30 characters";
-    error_message.innerHTML = text;
+    error_message_message.innerHTML = text;
+    document.getElementById("message").style.borderColor = "red";
     return false;
+  } else {
+    error_message_message.innerHTML = ""
+    document.getElementById("message").style.borderColor = "white";
   };
 
   alert("Form Submitted Successfully!");
-  document.querySelector('.contactUs').style.display = 'none';
   return true;
 };
 
